91dbc5873527fcebad1c67d823a08cfc7309131a
SamuelPaulos <samuel.paulos@yahoo.com>
2019-04-29 00:22:45 -0400

    first cmd less comment

---
+++ nocmd/src/main/java/com/example/nocmd/NocmdApplication.java
@@ -0,0 +1,13 @@
+package com.example.nocmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NocmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NocmdApplication.class, args);
+	}
+
+}
